cea4b463a7b15f37a62e088f521734574f7db34f
ALMEIDA Andre Alves <AFALMEIDA@SONAE.PT>
2021-01-05 22:14:05 +0000

    first commit

--- rp_tabs.py
+++ rp_tabs.py
@@ -1,23 +1,26 @@
 import plotly.express as px  # (version 4.7.0)
 import plotly
 import plotly.graph_objects as go
+import dash_table
 import pandas as pd
 import dash  # (version 1.12.0) pip install dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import base64
 from dash.dependencies import Input, Output
+import numpy as np
 
 #proximos passos:
-#1.melhorar design - check
-#2.explorar outros componentes
-#3.adicionar outros países - check
-#4.comparação entre países para a mesma categoria - next step
-#5.outro tab: selecionar a periodicidade, entre mensal, trimestral, e anual (aqui complica, porque tenho que calcular o ytd) - check
-
+#1.ajustar tamanho da pagina nos diferentes ecrãs
+#2.adicionar dados de preços
+#3.adicionar outros países
+#4.passar países a dropdown menu, com multi
+#5.passar para o pythonanywhere
 
 
 app = dash.Dash(__name__)
+
 
 # Import and clean data (importing csv into pandas)
 xls_m = pd.ExcelFile("retail_pulse_data.xls")
@@ -26,10 +29,7 @@ df_m_ESP = pd.read_excel(xls_m,'ESP')
 df_m_IT = pd.read_excel(xls_m,'IT')
 df_m_DE = pd.read_excel(xls_m,'DE')
 
-
 df_m = df_m_PT.append([df_m_ESP,df_m_IT,df_m_DE],ignore_index=True)
-df_m
-
 
 xls_q = pd.ExcelFile("retail_pulse_quarters.xls")
 df_q_PT = pd.read_excel(xls_q,'PT')
@@ -39,7 +39,6 @@ df_q_DE = pd.read_excel(xls_q,'DE')
 
 
 df_q = df_q_PT.append([df_q_ESP,df_q_IT,df_q_DE],ignore_index=True)
-df_q
 
 
 xls_y = pd.ExcelFile("retail_pulse_years.xls")
@@ -50,150 +49,238 @@ df_y_DE = pd.read_excel(xls_y,'DE')
 
 
 df_y = df_y_PT.append([df_y_ESP,df_y_IT,df_y_DE],ignore_index=True)
-df_y
 
-#1 App layout
+
+df_final = df_m
+df_final = df_final.append([df_q,df_y], ignore_index=True)
+df_final = df_final.round(1)
+
 app.layout = html.Div([
-html.H1("Retail pulse 2.0", style={'text-align': 'left'}),
-                dcc.RadioItems(id="slct_cntry",
-                    options=[
-                        {'label': 'Portugal', 'value': 'PT'},
-                        {'label': 'Spain', 'value': 'ES'},
-                        {'label': 'Germany', 'value': 'DE'},
-                        {'label': 'Italy', 'value': 'IT'},
+
+            dbc.Row([
+                dbc.Col(html.Img(src=app.get_asset_url('logo_retailpulse.png'),
+                                        style={'float': 'left'}
+                                ),
+                            ),
+       #             dbc.Col(html.Img(src=app.get_asset_url('logoSONAE.png'),
+       #                     style={#'height': '25%',
+       #                             #'width': '25%',
+       #                            'float': 'right',
+       #                            'margin-top': 20,
+       #                             'margin-right': 250
+       #                            }
+       #                      ),
+       #                ),
                         ],
-                    value= 'PT',
-                    labelStyle = {'display': 'inline-block'}
-                ),
+                    ),
+
+            html.Br(),
+            html.Br(),
+            html.Br(),
+
+                 dbc.Row(dbc.Col(html.H1("Retail Pulse"
+                                " ", style={'text-align': 'left'}),
+                                        width={'size': 3, 'offset': 1}
+                                        ),
+                                ),
 
 
-
-                html.Div(id='output_container', children=[]),
+                dbc.Row(dbc.Col(html.Div("Evolution of nominal retail sales in different "
+                                     "Euro area countries"),
+                            width={'size':3, 'offset': 1}
+                            )
+                    ),
 
                 html.Br(),
 
-                dcc.Graph(id='retail_sales_graph_month', figure={}),
+                dbc.Row(dbc.Col(html.Label(['Select country:'], style={'font-weight': 'bold'}),
+                                width={'size': 5, 'offset': 1},
+                                ),
+                        ),
+
+                dbc.Row(dbc.Col(dcc.RadioItems(id="slct_cntry",
+                                options=[
+                                    {'label': 'Portugal', 'value': 'PT'},
+                                    {'label': 'Spain', 'value': 'ES'},
+                                    {'label': 'Germany', 'value': 'DE'},
+                                    {'label': 'Italy', 'value': 'IT'},
+                                    ],
+                                value= 'PT',
+                                labelStyle = {'display': 'inline-block'}
+                                           ),
+                                        width={'size': 5 , 'offset': 1},
+                                        ),
+                                ),
+
+                dbc.Row(dbc.Col(html.Div(id='output_container_country', children=[]),
+                                width={'size': 5, 'offset': 1}
+                                ),
+                        ),
 
                 html.Br(),
 
-                dcc.Graph(id='retail_sales_graph_quarter', figure={}),
+
+                dbc.Row(dbc.Col(html.Label(['Select period:'], style={'font-weight': 'bold'}),
+                                width={'size': 5, 'offset': 1},
+                                ),
+                        ),
+
+                dbc.Row(dbc.Col(dcc.RadioItems(id="slct_period",
+                                                           options=[
+                                                               {'label': 'Monthly', 'value': 'MON'},
+                                                               {'label': 'Quarterly', 'value': 'QUA'},
+                                                               {'label': 'Annual', 'value': 'ANU'},
+                                                           ],
+                                                           value='MON',
+                                                           labelStyle={'display': 'inline-block'}
+                                                           ),
+                                            width={'size': 5, 'offset': 1},
+                                            ),
+                                    ),
+
+                dbc.Row(dbc.Col(html.Div(id='output_container_period', children=[]),
+                                width={'size': 5, 'offset': 1}
+                                ),
+                        ),
+
 
                 html.Br(),
 
-                dcc.Graph(id='retail_sales_graph_years', figure={})
+                dbc.Row(dbc.Col(dcc.Graph(id='the_graph', figure={}),
+                                    width={'size': 11, 'offset': 1}
+                                ),
+                        ),
+
+                html.Br(),
+
+                html.Br(),
+
+                html.Br(),
+
+                dbc.Row(dbc.Col(html.Div(id='table-container', className='tableDiv'),
+                        width={'size': 8, 'offset': 1})
+                        ),
+
+                html.Br(),
+
+                html.Br(),
+
+                dbc.Row(dbc.Col(html.Img(src=app.get_asset_url('logo_gspc.png')),
+                                width={'size': 10, 'offset': 2}
+                                ),
+                        ),
+
+                html.Br(),
+
+                html.Br(),
+
 
 ])
 
-
 #2 Connect the Plotly graphs with Dash Components
 @app.callback(
-        [Output(component_id='output_container', component_property='children'),
-         Output(component_id='retail_sales_graph_month', component_property='figure'),
-         Output(component_id='retail_sales_graph_quarter', component_property='figure'),
-         Output(component_id='retail_sales_graph_years', component_property='figure')         ],
-        [Input(component_id='slct_cntry', component_property='value')],
+        [Output(component_id='output_container_country', component_property='children'),
+        Output(component_id='output_container_period', component_property='children'),
+          Output(component_id='the_graph', component_property='figure'),
+         Output(component_id='table-container', component_property='children')],
+        [Input(component_id='slct_cntry', component_property='value'),
+         Input(component_id='slct_period', component_property='value')],
 )
 
 
-def update_graph(option_slctd):
+def update_graph(option_slctd, period_slctd):
+#1 App layout
         print(option_slctd)
         print(type(option_slctd))
 
-        container = "The country chosen by user was: {}".format(option_slctd)
+        container_country  = "The country chosen by user was: {}".format(option_slctd)
+        container_period = "The period selected by user was: {}".format(period_slctd)
 
-        dff_m = df_m.copy()
-        dff_m = dff_m[dff_m["country"] == option_slctd]
-
-        dff_q = df_q.copy()
-        dff_q = dff_q[dff_q["country"] == option_slctd]
-
-        dff_y = df_y.copy()
-        dff_y = dff_y[dff_y["country"] == option_slctd]
-
+        dff_final = df_final.copy()
+        dff_final = dff_final[dff_final["country"] == option_slctd]
+        dff_final = dff_final[dff_final["periodicidade"] == period_slctd]
+        #dff_final.to_excel(r'dff_final.xlsx', index=False)
+        dff_final.time = pd.DatetimeIndex(dff_final.time).strftime("%Y-%m-%d")
+        fig = px.line(data_frame=dff_final,
+                    x='time',
+                    y=['Total','Food','Food in non-specialized stores','Food in specialized stores','Non-food','Fuel','Audio and video equipment and household appliances','Fashion','Computers, peripheral units and software','Healthcare','Eletronics'],
+                    template='seaborn',
+                    title="<b>Nominal retail sales - growth rate (YoY)<b>",
+                    labels={
+                        'time': 'time',
+                        'y': 'categories'
+                }
+            )
         #3 Ploty
-        fig_m = px.line(data_frame=dff_m,
-                    x='time',
-                    y=['Total','Food','Food in non-specialized stores','Food in specialized stores','Non-food','Fuel','Audio and video equipment and household appliances','Fashion','Computers, peripheral units and software','Healthcare','Eletronics'],
-                    template='seaborn',
-                    title="<b>Monthly retail sales - growth rate (YoY)<b>",
-                    labels={
-                        'time': 'time',
-                        'y': 'categories'
-                }
-            )
-        fig_q = px.line(data_frame=dff_q,
-                    x='time',
-                    y=['Total','Food','Food in non-specialized stores','Food in specialized stores','Non-food','Fuel','Audio and video equipment and household appliances','Fashion','Computers, peripheral units and software','Healthcare','Eletronics'],
-                    template='seaborn',
-                    title="<b>Quarterly retail sales - growth rate (YoY)<b>",
-                    labels={
-                        'time': 'time',
-                        'y': 'categories'
-                }
-            )
 
-
-        fig_y = px.line(data_frame=dff_y,
-                    x='time',
-                    y=['Total','Food','Food in non-specialized stores','Food in specialized stores','Non-food','Fuel','Audio and video equipment and household appliances','Fashion','Computers, peripheral units and software','Healthcare','Eletronics'],
-                    template='seaborn',
-                    title="<b>Annual retail sales - growth rate (YoY)<b>",
-                    labels={
-                        'time': 'time',
-                        'y': 'categories'
-                }
-            )
-
-        fig_m.update_traces(mode='markers+lines')
-        fig_m.update_xaxes(rangeslider_visible=True)
+        fig.update_traces(mode='markers+lines', hovertemplate=None)
+        fig.update_xaxes(rangeslider_visible=True, showspikes=True)
+        fig.update_yaxes(title="Percantage", showspikes=True),
         #plotly.offline.plot(fig, filename="Retail_Pulse.html")
-        fig_m.update_layout(  # customize font and legend orientation & position
+        fig.update_layout(  # customize font and legend orientation & position
             font_family="Calibri",
+            hovermode="x",
             legend=dict(
-            title=None, orientation="h", y=1, yanchor="bottom", x=0.5, xanchor="center"
-           )
+            title=None, orientation="h", y=-0.5, yanchor="top", x=0.5, xanchor="center", font = dict (size=18)
+           ),
         )
-        fig_m.update_layout(
+        fig.update_layout(
             autosize=False,
             width=1280,
             height=720,
             margin=dict(l=125, r=125, t=125, b=50),
             )
-        #plotly.offline.plot(fig_m, filename="Retail_Pulse_months.html")
 
-        fig_q.update_traces(mode='markers+lines')
-        fig_q.update_xaxes(rangeslider_visible=True)
-        fig_q.update_layout(  # customize font and legend orientation & position
-                font_family="Calibri",
-                legend=dict(
-                title=None, orientation="h", y=1, yanchor="bottom", x=0.5, xanchor="center"
-           )
-        )
-        fig_q.update_layout(
-            autosize=False,
-            width=1280,
-            height=720,
-            margin=dict(l=125, r=125, t=125, b=50),
-            )
-        #plotly.offline.plot(fig_q, filename="Retail_Pulse_quarters.html")
 
-        fig_y.update_traces(mode='markers+lines')
-        fig_y.update_xaxes(rangeslider_visible=True)
-        fig_y.update_layout(  # customize font and legend orientation & position
-            font_family="Calibri",
-            legend=dict(
-            title=None, orientation="h", y=1, yanchor="bottom", x=0.5, xanchor="center"
-           )
-        )
-        fig_y.update_layout(
-            autosize=False,
-            width=1280,
-            height=720,
-            margin=dict(l=125, r=125, t=125, b=50),
-            ),
-        #plotly.offline.plot(fig_y, filename="Retail_Pulse_annual.html")
+        categories_to_hide = ['Food','Food in non-specialized stores','Food in specialized stores','Non-food','Fuel','Audio and video equipment and household appliances','Fashion','Computers, peripheral units and software','Healthcare','Eletronics']
+        fig.for_each_trace(lambda trace: trace.update(visible="legendonly")
+                   if trace.name in categories_to_hide else ())
 
-        return container, fig_m, fig_q, fig_y
+
+
+
+        tab = dash_table.DataTable(
+                id='table',
+                columns=[
+                    {"name": i, "id": i, "deletable": True, "selectable": True, "hideable": True}
+                    for i in df_final.columns
+                ],
+                data=dff_final.to_dict('records'),
+                editable=False,
+                export_format='xlsx',
+                export_columns='visible',
+                style_cell={'textAlign': 'center', 'font_size': '16px'},
+                style_data_conditional=[
+                    {
+                        'if': {'row_index': 'odd'},
+                        'backgroundColor': 'rgb(248, 248, 248)',
+                        'minWidth': 95, 'maxWidth': 295, 'width': 150,
+                    }
+                ],
+                style_header={
+                    'backgroundColor': '#e0ffff',
+                    'fontWeight': 'bold',
+                    'textAlign': 'left',
+                    'minWidth': 200, 'maxWidth': 600, 'width': 400,
+                    'whiteSpace': 'normal',
+                    'height': 'auto',
+                },
+                filter_action="none",
+                sort_action="native",
+                sort_mode="multi",
+                column_selectable="multi",
+                row_selectable="multi",
+                row_deletable=False,
+                selected_columns=[],
+                selected_rows=[],
+                page_action="native",  # all data is passed to the table up-front or not ('none')
+                page_current=0,
+                page_size=10,
+                style_table={'overflowX': 'auto'},
+        ),
+
+
+        return container_country, container_period, fig, tab
 
 
 # ------------------------------------------------------------------------------
